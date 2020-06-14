607ad480308dedd9c8a03d0b7a9f26cadd9a442e
Tom Scharstein <trscharstein@gmail.com>
2020-06-14 00:28:55 -0700

    Update dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
 // Vue.config.devtools = false;
 
+// Brutalism library support
 import {
   Anno,
   Alert,
@@ -30,7 +31,9 @@ import {
   Tabs,
   TextArea,
   Toggle,
-  Wrapper
+  Wrapper,
+  FilePicker,
+  ColorPicker,
 } from "brutalism";
 
 Vue.component("Anno", Anno);
@@ -42,6 +45,8 @@ Vue.component("Divider", Divider);
 Vue.component("Dropdown", Dropdown);
 Vue.component("Dropzone", Dropzone);
 Vue.component("File-Input", FileInput);
+Vue.component("File-Picker", FilePicker);
+Vue.component("Color-Picker", ColorPicker);
 Vue.component("Fold", Fold);
 Vue.component("Footer", Footer);
 Vue.component("Grid", Grid);
@@ -61,5 +66,5 @@ Vue.component("Wrapper", Wrapper);
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount("#app");
